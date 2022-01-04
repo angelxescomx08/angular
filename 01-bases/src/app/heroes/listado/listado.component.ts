663f2acf07e2825685ce1f9268195f2152c329fd
ahxdevelopmx <ah@xdevelop.mx>
2022-01-04 17:35:59 -0600

    modulos, directivas *ngIf,*ngFor

---
+++ 01-bases/src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent{
+
+  heroes: string[] = ['Spiderman','Ironman','Hulk','Thor'];
+  heroeBorrado: string = '';
+
+  constructor() { }
+
+  borrarHeroe():void{
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
+}
